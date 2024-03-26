9960780f787e4489fdb41f87d122b65763aef4e4
Britanico313 <144167838+Britanico313@users.noreply.github.com>
2024-03-26 01:57:25 +0000

    sdsd

---
+++ hello-react/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Test in Text For Professor Siarto -- TRY THE LINK C:
+        </p>
+        <a
+          className="App-link"
+          href="https://siarto.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FOR MI449
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
